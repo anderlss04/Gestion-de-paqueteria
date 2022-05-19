9291b6b951a5851a382b1d1468d84fe3f073a854
Ander <91115165+anderlss04@users.noreply.github.com>
2022-05-19 20:40:58 +0200

    borrar codigo comentado

--- src/controlador/Programa.java
+++ src/controlador/Programa.java
@@ -8,23 +8,13 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.SAXException;
 
 public class Programa {
-	public String user="";
+	
 	
 	public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException {
 		var f = new Controlador();
 		boolean correcto = false;
-		
-//		f.generarXML();
-		
-//		while(!f.correcto) {
+
 			f.login();
-
-//			if(f.correcto)
-//				f.vistaMenu();
-//			else
-//				JOptionPane.showMessageDialog(null, "El usuario o la contraseña no coinciden \nRevise bien los datos porfavor");
-//		}
-
 
 	}
 
